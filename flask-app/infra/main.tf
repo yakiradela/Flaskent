2f5955e00c46a27184d5e66b31180762efc6b075
Yakir Adela <yakirade@gmail.com>
2025-05-18 18:16:47 +0300

    Update main.tf

--- flask-app/infra/main.tf
+++ flask-app/infra/main.tf
@@ -1,6 +1,9 @@
-# יצירת הדלי תחילה
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
+  bucket = "terraform-state--bucketxyz123"
+}
+
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  bucket = aws_s3_bucket.terraform_state.id
   acl    = "private"
 }
 
