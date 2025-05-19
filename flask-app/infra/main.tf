c6a616a6c8accbf4c5bb597601898c53fe42a966
Yakir Adela <yakirade@gmail.com>
2025-05-19 17:35:24 +0300

    Update main.tf

--- flask-app/infra/main.tf
+++ flask-app/infra/main.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state--bucketxyz123"
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#  bucket = "terraform-state--bucketxyz123"
+# }
 
 resource "aws_s3_bucket_acl" "terraform_state_acl" {
   bucket = aws_s3_bucket.terraform_state.id
