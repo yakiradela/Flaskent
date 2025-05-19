a5b1eeb16f641e68d5f3524f77fd3d2fccfa2eea
Yakir Adela <yakirade@gmail.com>
2025-05-19 22:26:51 +0300

    Update iam.tf

--- flask-app/infra/iam.tf
+++ flask-app/infra/iam.tf
@@ -87,5 +87,9 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_read_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_ssm_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
 
