3bf80ad433613d20ab6f1750fd8ba6af59f0dbf1
Yakir Adela <yakirade@gmail.com>
2025-05-19 19:26:40 +0300

    Update outputs.tf

--- flask-app/infra/bootstrap/outputs.tf
+++ flask-app/infra/bootstrap/outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_name" {
-  value = module.bootstrap.s3_bucket_name
+  value = aws_s3_bucket.terraform_state.bucket
 }
 
 output "dynamodb_table_name" {
-  value = module.bootstrap.dynamodb_table_name
+  value = aws_dynamodb_table.terraform_locks.name
 }
