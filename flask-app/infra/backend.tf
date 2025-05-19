13e80732e9dfbde0f5b0ebf87577c3c64908efa8
Yakir Adela <yakirade@gmail.com>
2025-05-19 18:01:23 +0300

    Update backend.tf

--- flask-app/infra/backend.tf
+++ flask-app/infra/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state--bucketxyz123"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-  }
-}
+# terraform {
+  # backend "s3" {
+    # bucket         = "terraform-state--bucketxyz123"
+    # key            = "eks/terraform.tfstate"
+    # region         = "us-east-2"
+    # encrypt        = true
+  # }
+# }
