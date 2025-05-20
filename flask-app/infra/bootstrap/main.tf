aecec6b2e2f3f6087f09235e113b92dff9fb08f1
Yakir Adela <yakirade@gmail.com>
2025-05-21 00:34:55 +0300

    Update main.tf

--- flask-app/infra/bootstrap/main.tf
+++ flask-app/infra/bootstrap/main.tf
@@ -1,1 +1,19 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state--bucketxyz123"
+  tags = {
+    Name        = "Terraform State"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
